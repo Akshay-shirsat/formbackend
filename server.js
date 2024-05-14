d30a60a9601980ee32341b8e103ff0b026af4243
AKSHAY SHIRSAT <arsofficial28@gmail.com>
2024-05-14 11:03:05 +0530

    updated

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require("express")
 const mongoose = require("mongoose")
 const morgan= require("morgan")
 const bodyParser = require("body-parser")
+const cors = require('cors');
 
 const UsersRoute = require('./routes/signupformroute')
+const app=express()
+
+
+
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 
 mongoose.connect('mongodb://localhost:27017/testdb',{useNewUrlParser:true,useUnifiedTopology:true})
 const db = mongoose.connection
-
 
 db.on('error',(err)=>{
     console.log(err)
@@ -17,16 +26,16 @@ db.once('open',()=>{
     console.log('database connection established ')
 })
 
-const app=express()
+app.use('/api/users',UsersRoute)
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+
+app.use((err, req, res, next) => {
+    console.error('Error:', err);
+    res.status(500).send('Internal Server Error');
+});
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
-
-app.use('/api/users',UsersRoute)
